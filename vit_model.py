a902f0c8fcbbc5b72bb1c1a3ec54305977115c9d
B Phan <bphan@Bs-MacBook-Pro.local>
2023-10-19 17:28:04 -0600

    fixed cropping dimensions

--- vit_model.py
+++ vit_model.py
@@ -8,13 +8,8 @@ class PatchEmbeddings(nn.Module):
     """TODO: (0.5 out of 8) Calculates patch embedding
     of shape `(batch_size, seq_length, hidden_size)`.
     """
-    def __init__(
-            self, 
-            image_size: int,
-            patch_size: int,
-            hidden_size: int,
-            num_channels: int = 3,      # 3 for RGB, 1 for Grayscale
-        ):
+    # num_channels = 3 for RGB, 1 for Grayscale
+    def __init__(self, image_size: int, patch_size: int, hidden_size: int, num_channels: int = 3):
         super().__init__()
         # #########################
         # Finish Your Code HERE
@@ -29,14 +24,8 @@ class PatchEmbeddings(nn.Module):
         self.patch_size = patch_size
         self.num_channels = num_channels
         self.num_patches = num_patches
-    
-    def getNumPatches(self):
-        return self.num_patches
 
-    def forward(
-            self, 
-            x: torch.Tensor,
-        ) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size, num_channels, height, width = x.shape
         if num_channels != self.num_channels:
             raise ValueError(
@@ -48,20 +37,17 @@ class PatchEmbeddings(nn.Module):
         # #########################
 
         # Calculate Patch Embeddings, then flatten into
-        # batched 1D sequence (batch_size, seq_length, hidden_size)
-        x = x.view(batch_size, num_channels, height // self.patch_size, self.patch_size, width // self.patch_size, self.patch_size)
+        # batched 1D sequence (batch_size, seq_length, hidden size)
+        x = x.view(batch_size, self.num_channels, height // self.patch_size, self.patch_size, width // self.patch_size, self.patch_size)
         x = x.permute(0, 2, 4, 1, 3, 5)
-        x = x.reshape(batch_size, -1, num_channels * self.patch_size * self.patch_size)
+        x = x.flatten(1, 2) # [batch_size, num of patches, self.num_channels, self.patch_size, self.patch_size]
+        x = x.flatten(2, 4) # [batch_size, seq_length, hidden size]
         embeddings = self.projection(x)
         # #########################
         return embeddings
 
 class PositionEmbedding(nn.Module):
-    def __init__(
-            self,
-            num_patches,
-            hidden_size,
-        ):
+    def __init__(self, num_patches, hidden_size):
         """TODO: (0.5 out of 8) Given patch embeddings, 
         calculate position embeddings with [CLS] and [POS].
         """
@@ -69,29 +55,29 @@ class PositionEmbedding(nn.Module):
         # #########################
         # Finish Your Code HERE
         # #########################
+
+        self.hidden_size = hidden_size
+        self.num_patches = num_patches
         
         # Specify [CLS] and positional embedding as learnable parameters
 
-        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden_size))
-        self.position_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, hidden_size))
+        self.cls_token = nn.Parameter(torch.rand(1, hidden_size))
+        self.position_embeddings = nn.Parameter(torch.randn(1, 1 + self.num_patches, self.hidden_size))
+        self.position_embeddings.requires_grad = False
 
         # #########################
 
-    def forward(
-            self,
-            embeddings: torch.Tensor
-        ) -> torch.Tensor:
+    def forward(self, embeddings: torch.Tensor) -> torch.Tensor:
         # #########################
         # Finish Your Code HERE
         # #########################
 
         # Concatenate [CLS] token with embedded patch tokens
-        cls_token = self.cls_token.expand(embeddings.shape[0], -1, -1)
-        embeddings = torch.cat([cls_token, embeddings], dim=1)
+        embeddings = torch.stack([torch.vstack((self.cls_token, embeddings[i])) for i in range(len(embeddings))])
+
         # Then add positional encoding to each token
-        if embeddings.size(1) > self.position_embeddings.size(1):
-            embeddings = embeddings[:, :self.position_embeddings.size(1), :]
-        embeddings += self.position_embeddings
+        embeddings = embeddings + self.position_embeddings[:,:self.num_patches + 1]
+
         # #########################
         return embeddings
 
@@ -108,15 +94,17 @@ class ResidualAttentionBlock(nn.Module):
         # Finish Your Code HERE
         # #########################
 
-        self.attn = nn.MultiheadAttention(d_model, n_head)    # Refer to nn.MultiheadAttention
         self.ln_1 = nn.LayerNorm(d_model)
-        self.mlp = nn.Sequential(
-            nn.Linear(d_model, 2 * d_model),
-            nn.GELU(),
-            nn.Linear(2 * d_model, d_model)
-        )     # A trick is to use nn.Sequential to specify multiple layers at once
+        self.attn = nn.MultiheadAttention(d_model, n_head)    # Refer to nn.MultiheadAttention
         self.ln_2 = nn.LayerNorm(d_model)
-
+        # A trick is to use nn.Sequential to specify multiple layers at once
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model, 4 * d_model), 
+            nn.GELU(), 
+            nn.Dropout(0.2),
+            nn.Linear(4 * d_model, d_model),
+            nn.Dropout(0.2)
+        )
         # #########################
 
     def forward(self, x: torch.Tensor):
@@ -126,12 +114,13 @@ class ResidualAttentionBlock(nn.Module):
         # #########################
 
         # LayerNorm -> Multi-head Attention
-        inp_x = self.ln_1(x)
-        x = x + self.attn(inp_x, inp_x, inp_x)[0]
+        inp_x_1 = self.ln_1(x)
         # Residual connection against x
+        x = x + self.attn(inp_x_1, inp_x_1, inp_x_1)[0]
         # LayerNorm -> MLP Block
+        inp_x_2 = self.ln_2(x)
         # Residual connection against x
-        out = x + self.mlp(self.ln_2(x))
+        out = x + self.mlp(inp_x_2)
         # #########################
 
         return out
@@ -152,30 +141,22 @@ class Transformer(nn.Module):
 class ViT(nn.Module):
     """TODO: (0.5 out of 8) Vision Transformer.
     """
-    def __init__(
-            self, 
-            image_size: int, 
-            patch_size: int, 
-            num_channels: int,
-            hidden_size: int, 
-            layers: int, 
-            heads: int,
-        ):
+    def __init__(self, image_size: int, patch_size: int, num_channels: int, hidden_size: int, layers: int, heads: int):
         super().__init__()
         self.hidden_size = hidden_size
         # #########################
         # Finish Your Code HERE
         # #########################
-        self.patch_embed = PatchEmbeddings(image_size, patch_size, hidden_size, num_channels)
-        num_patches = self.patch_embed.getNumPatches()
-        self.pos_embed = PositionEmbedding(num_patches, hidden_size)
+        self.patch_embed = PatchEmbeddings(image_size, patch_size, self.hidden_size, num_channels)
 
-        self.ln_pre = nn.LayerNorm(hidden_size)
+        self.pos_embed = PositionEmbedding(self.patch_embed.num_patches, self.hidden_size)
 
-        self.transformer = Transformer(hidden_size, layers, heads)  # TODO: Use the provided transformer codeblock
+        self.ln_pre = nn.LayerNorm(self.hidden_size)
 
-        self.ln_post = nn.LayerNorm(hidden_size)
-            
+        self.transformer = Transformer(self.hidden_size, layers, heads)     # TODO: Use the provided transformer codeblock
+
+        self.ln_post = nn.LayerNorm(self.hidden_size)
+
         # #########################
 
 
@@ -184,26 +165,22 @@ class ViT(nn.Module):
         # Finish Your Code HERE
         # #########################
         x = self.patch_embed(x)
-        x = self.pos_embed(x)
         x = self.ln_pre(x)
+        x = self.pos_embed(x)
+        # #########################
         x = self.transformer(x)
         out = self.ln_post(x)
         out = out[:, 0, :]
-        # #########################
         return out
 
 class ClassificationHead(nn.Module):
-    def __init__(
-            self, 
-            hidden_size: int,
-            num_classes: int = 10,
-        ):
+    def __init__(self, hidden_size: int, num_classes: int = 10):
         super().__init__()
         self.classifier = nn.Linear(hidden_size, num_classes)
 
     def forward(
-            self, 
-            feats: torch.Tensor,
+        self, 
+        feats: torch.Tensor,
         ) -> torch.Tensor:
         out = self.classifier(feats)
         return out
@@ -211,11 +188,7 @@ class ClassificationHead(nn.Module):
 class LinearEmbeddingHead(nn.Module):
     """TODO: (0.25 out of 8) Given features from ViT, generate linear embedding vectors.
     """
-    def __init__(
-            self, 
-            hidden_size: int,
-            embed_size: int = 64,
-        ):
+    def __init__(self, hidden_size: int, embed_size: int = 64):
         super().__init__()
         self.embed_size = embed_size
         # #########################
@@ -224,10 +197,7 @@ class LinearEmbeddingHead(nn.Module):
         self.projection = nn.Linear(hidden_size, embed_size)
         # #########################
 
-    def forward(
-        self, 
-        feats: torch.Tensor,
-        ) -> torch.Tensor:
+    def forward(self, feats: torch.Tensor) -> torch.Tensor:
         # #########################
         # Finish Your Code HERE
         # #########################
